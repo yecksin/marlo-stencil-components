0ba5818ed27ec6907109d1e4632463f4160d4943
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-04-04 14:53:59 -0500

    :sparkles: feat: dist added

---
+++ dist/cjs/loader.cjs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var index = require('./index-BZ7EVjNV.js');
+var appGlobals = require('./app-globals-V2Kpy_OQ.js');
+
+const defineCustomElements = async (win, options) => {
+  if (typeof window === 'undefined') return undefined;
+  await appGlobals.globalScripts();
+  return index.bootstrapLazy([["my-component.cjs",[[1,"my-component",{"first":[1],"middle":[1],"last":[1]}]]]], options);
+};
+
+exports.setNonce = index.setNonce;
+exports.defineCustomElements = defineCustomElements;
+//# sourceMappingURL=loader.cjs.js.map
+
+//# sourceMappingURL=loader.cjs.js.map
